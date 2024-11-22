e337282be5076e6a24a8d7df1bf80e37121fa4fa
yuzhihao <17150515155@163.com>
2024-11-22 13:56:44 +0800

    init

---
+++ src/main/java/com/yuzhihao/learn/vlcj/MediaVlc.java
@@ -0,0 +1,88 @@
+package com.yuzhihao.learn.vlcj;
+
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.javafx.videosurface.ImageViewVideoSurface;
+import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+
+public class MediaVlc {
+
+    private final MediaPlayerFactory mediaPlayerFactory;
+
+    private final EmbeddedMediaPlayer embeddedMediaPlayer;
+
+    private final ImageView videoImageView;
+
+    public MediaVlc() {
+        this.mediaPlayerFactory = new MediaPlayerFactory();
+        this.embeddedMediaPlayer = mediaPlayerFactory.mediaPlayers().newEmbeddedMediaPlayer();
+        this.embeddedMediaPlayer.events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
+
+            @Override
+            public void playing(MediaPlayer mediaPlayer) {
+            }
+
+            @Override
+            public void paused(MediaPlayer mediaPlayer) {
+            }
+
+            @Override
+            public void stopped(MediaPlayer mediaPlayer) {
+            }
+
+            @Override
+            public void timeChanged(MediaPlayer mediaPlayer, long newTime) {
+            }
+        });
+
+        this.videoImageView = new ImageView();
+        this.videoImageView.setPreserveRatio(true);
+
+        embeddedMediaPlayer.videoSurface().set(new ImageViewVideoSurface(this.videoImageView));
+    }
+
+
+    public final void start(Stage primaryStage) throws Exception {
+
+        BorderPane root = new BorderPane();
+        root.setStyle("-fx-background-color: black;");
+
+        videoImageView.fitWidthProperty().bind(root.widthProperty());
+        videoImageView.fitHeightProperty().bind(root.heightProperty());
+
+        root.widthProperty().addListener((observableValue, oldValue, newValue) -> {
+            // If you need to know about resizes
+        });
+
+        root.heightProperty().addListener((observableValue, oldValue, newValue) -> {
+            // If you need to know about resizes
+        });
+
+        root.setCenter(videoImageView);
+
+        Scene scene = new Scene(root, 1200, 675, Color.BLACK);
+        primaryStage.setTitle("vlcj JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+//        embeddedMediaPlayer.media().play(params.get(0));
+//        embeddedMediaPlayer.media().play("http://192.168.4.231:7080/34020000001320000001/1050002775.live.flv");
+        embeddedMediaPlayer.media().play("rtmp://192.168.4.231:7935/34020000001320000001/1050002775");
+
+        embeddedMediaPlayer.controls().setPosition(0.4f);
+    }
+
+    public final void stop() {
+        embeddedMediaPlayer.controls().stop();
+        embeddedMediaPlayer.release();
+        mediaPlayerFactory.release();
+    }
+
+
+}
---
+++ src/test/java/com/yuzhihao/learn/vlcj/VlcjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuzhihao.learn.vlcj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlcjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
