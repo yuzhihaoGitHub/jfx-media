1b95b1cd5f507a6f1456b6fe7e75b6bad11b7573
yuzhihao <17150515155@163.com>
2024-12-10 10:58:21 +0800

    feat：程序打包时排出不必要的lib

--- src/main/java/com/yuzhihao/learn/config/VlcRuntime.java
+++ src/main/java/com/yuzhihao/learn/config/VlcRuntime.java
@@ -1,15 +1,25 @@
 package com.yuzhihao.learn.config;
 
+import lombok.extern.log4j.Log4j2;
+
 /**
  *
  *
  * @author yuzhihao
  */
+@Log4j2
 public class VlcRuntime {
 
     public final static String JAVA_HOME = System.getProperty("java.home");
     public final static String USER_DIR = System.getProperty("user.dir");
     public final static String USER_HOME = System.getProperty("user.home");
     public final static String APPDATA = System.getenv("APPDATA");
+
+    static {
+        log.info("java.home:{}", JAVA_HOME);
+        log.info("user.dir:{}", USER_DIR);
+        log.info("user.home:{}", USER_HOME);
+        log.info("APPDATA:{}", APPDATA);
+    }
 
 }
